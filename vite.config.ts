586d57b4df826f1b26ff99b04188286676a87559
dashi296 <falcon0296@gmail.com>
2024-07-13 23:42:33 +0900

    [update] package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc';
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: './lib/main.ts',
-      name: 'Counter',
-      fileName: 'counter'
+      name: 'Confirm',
+      fileName: 'index',
+      formats: ["es", "umd"]
     }
   }
 })
