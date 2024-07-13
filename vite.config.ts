04cd37ddc8065f5c882e30e576aa22be0d53a860
dashi296 <falcon0296@gmail.com>
2024-07-14 01:22:34 +0900

    [fix] main.tsをindex.tsに

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), dts()],
   build: {
     lib: {
-      entry: resolve(__dirname, './lib/main.ts'),
+      entry: resolve(__dirname, './lib/index.ts'),
       name: '@dashi296/react-confirm',
       fileName: 'index',
       formats: ["es", "umd"]
